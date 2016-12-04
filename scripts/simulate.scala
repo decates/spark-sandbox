955c5c73a667586e9e4070508e9330e1b573c5b2
Mark de Cates <decates@gmail.com>
2016-12-04 23:06:35 +0000

    FEAT: Most of data prep complete for learning.

--- scripts/simulate.scala
+++ scripts/simulate.scala
@@ -1,11 +1,10 @@
-val a = 5
-val b = 3
-val c = a + b
-
 import org.apache.spark.sql._
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions.udf
 
 val dataFilePath = "/home/vagrant/data/lse/LSE*.txt"
+val learningOffsets = Seq(-1, -2, -3, -5, -10, -30)
+val predictionOffset = 30
 
 def loadMetastock(dataFilePath: String): Dataset[Row] = {
   val metaStockSevenDailySchema =
@@ -30,8 +29,29 @@ def buildDateIndex(data: Dataset[Row], dateColumnName: String): Dataset[Row] = {
     StructType(
       StructField("date", StringType, false) ::
       StructField("index", LongType, false) :: Nil)
-  spark.createDataFrame(dateRDD, dateSchema).limit(10)
+  spark.createDataFrame(dateRDD, dateSchema)
 }
+
+def addOffset(dataToAddTo: Dataset[Row], originalData: Dataset[Row], offset: Int): Dataset[Row] = {
+  // Create a copy with an offset index
+  var offsetDF = originalData.withColumn("index", originalData("index") + offset)
+  // Rename the offset columns to avoid duplicate column names
+  val extraColumns = offsetDF.columns.filter(c => (c != "index" && c != "ticker")).foreach(c => {
+    offsetDF = offsetDF.withColumnRenamed(c, c + offset.toString)
+  })
+  // data("index") === offsetDF("index")
+  dataToAddTo.join(offsetDF, List("index", "ticker"), "inner")
+}
+
+def combineWithOffsets(data: Dataset[Row], offsets: Seq[Int]): Dataset[Row] = {
+  offsets.foldLeft(data)((d, o) => addOffset(d, data, o))
+}
+
+def priceDifference(currentPrice: Float, futurePrice: Float):Float = {
+  futurePrice - currentPrice
+}
+
+val priceDifferenceUdf = udf(priceDifference(_:Float,_:Float):Float)
 
 val stockDF = loadMetastock(dataFilePath)
 stockDF.createOrReplaceTempView("stocks")
@@ -40,5 +60,26 @@ stockDF.createOrReplaceTempView("stocks")
 val dateDF = buildDateIndex(stockDF, "date")
 dateDF.createOrReplaceTempView("dates")
 //dateDF.show()
+//spark.sql("SELECT close, index FROM stocks JOIN dates ON stocks.date = dates.date WHERE ticker = 'MARS'").show()
 
-spark.sql("SELECT close, index FROM stocks JOIN dates ON stocks.date = dates.date WHERE ticker = 'MARS'").show()
+val stockWithIndexDF = spark.sql("SELECT index, ticker, open, high, low, close, volume FROM stocks JOIN dates ON stocks.date = dates.date")//.where("ticker = 'MARS'")
+val filteredDF = stockWithIndexDF//.filter("index < 100")
+filteredDF.cache()
+//stockWithIndexDF.show()
+//stockWithIndexDF.filter("index > 30").filter("ticker = 'MARS'")
+val joinedData = combineWithOffsets(filteredDF, learningOffsets)
+joinedData.filter("index = 0").show()
+
+val dataForCalculationResponses = stockWithIndexDF.select("index", "ticker", "close");
+val possibleResponses = (
+  addOffset(dataForCalculationResponses, dataForCalculationResponses, predictionOffset)
+  withColumnRenamed("close", "original_close")
+  withColumnRenamed("close" + predictionOffset, "future_close")
+  withColumn("price_difference", priceDifferenceUdf($"original_close", $"future_close"))
+  drop("original_close")
+  drop("future_close"))
+possibleResponses.show()
+
+// Align training data (X) and expected responses (Y) ready for passing to model
+
+// Split on ticker into training, cross-validation and test sets (if h2o doesn't do it already?)
