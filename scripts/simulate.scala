97e6a1a12f75d3d070dffa6592adf1baf11f1e5c
Mark de Cates <decates@gmail.com>
2016-12-05 21:57:50 +0000

    FEAT: Made price changes relative to 'current'.

--- scripts/simulate.scala
+++ scripts/simulate.scala
@@ -4,7 +4,8 @@ import org.apache.spark.sql.functions.udf
 
 
 val dataFilePath = "/home/vagrant/data/lse/LSE*.txt"
-val learningOffsets = Seq(-1, -2, -3, -5, -10, -30)
+val responseColumnName = "price_change_percent"
+val learningOffsets = Seq(-1, -2, -3, -5, -6, -7, -8, -9, -10, -12, -15, -20, -30, -50)
 val predictionOffset = 30
 
 def loadMetastock(dataFilePath: String): Dataset[Row] = {
@@ -33,57 +34,58 @@ def buildDateIndex(data: Dataset[Row], dateColumnName: String): Dataset[Row] = {
   spark.createDataFrame(dateRDD, dateSchema)
 }
 
+def difference(a: Float, b: Float):Float = {
+  b - a
+}
+
+def percentChange(originalValue: Float, newValue: Float):Float = {
+  ((newValue - originalValue) / originalValue) * 100
+}
+
+val differenceUdf = udf(difference(_:Float,_:Float):Float)
+val percentChangeUdf = udf(percentChange(_:Float, _:Float):Float)
+
 def addOffset(dataToAddTo: Dataset[Row], originalData: Dataset[Row], offset: Int): Dataset[Row] = {
   // Create a copy with an offset index
   var offsetDF = originalData.withColumn("index", originalData("index") + offset)
   // Rename the offset columns to avoid duplicate column names
-  val extraColumns = offsetDF.columns.filter(c => (c != "index" && c != "ticker")).foreach(c => {
+  offsetDF.columns.filter(c => (c != "index" && c != "ticker")).foreach(c => {
     offsetDF = offsetDF.withColumnRenamed(c, c + offset.toString)
   })
-  dataToAddTo.join(offsetDF, List("index", "ticker"), "inner")
+  val combinedData = dataToAddTo.join(offsetDF, List("index", "ticker"), "inner")
+
+  // Make price data relative to the original
+  combinedData.withColumn("close" + offset.toString, percentChangeUdf($"close", col("close"+offset.toString)))
 }
 
 def combineWithOffsets(data: Dataset[Row], offsets: Seq[Int]): Dataset[Row] = {
   offsets.foldLeft(data)((d, o) => addOffset(d, data, o))
 }
 
-def priceDifference(currentPrice: Float, futurePrice: Float):Float = {
-  futurePrice - currentPrice
-}
-
-val priceDifferenceUdf = udf(priceDifference(_:Float,_:Float):Float)
-
 val stockDF = loadMetastock(dataFilePath)
 stockDF.createOrReplaceTempView("stocks")
-//stockDF.show()
 
 val dateDF = buildDateIndex(stockDF, "date")
 dateDF.createOrReplaceTempView("dates")
-//dateDF.show()
-//spark.sql("SELECT close, index FROM stocks JOIN dates ON stocks.date = dates.date WHERE ticker = 'MARS'").show()
 
-val stockWithIndexDF = spark.sql("SELECT index, ticker, open, high, low, close, volume FROM stocks JOIN dates ON stocks.date = dates.date")//.where("ticker = 'MARS'")
-val filteredDF = stockWithIndexDF//.filter("index < 100")
+val stockWithIndexDF = spark.sql("SELECT index, ticker, close, volume FROM stocks JOIN dates ON stocks.date = dates.date")//.where("ticker = 'MARS'").where("index > 100").where("index < 200")
+val filteredDF = stockWithIndexDF
 filteredDF.cache()
-//stockWithIndexDF.show()
-//stockWithIndexDF.filter("index > 30").filter("ticker = 'MARS'")
 val joinedData = combineWithOffsets(filteredDF, learningOffsets)
 joinedData.filter("index = 0").show()
 
 val dataForCalculationResponses = stockWithIndexDF.select("index", "ticker", "close");
 val possibleResponses = (
   addOffset(dataForCalculationResponses, dataForCalculationResponses, predictionOffset)
-  withColumnRenamed("close", "original_close")
-  withColumnRenamed("close" + predictionOffset, "future_close")
-  withColumn("price_difference", priceDifferenceUdf($"original_close", $"future_close"))
-  drop("original_close")
-  drop("future_close"))
+  withColumn(responseColumnName, col("close" + predictionOffset))
+  drop("close" + predictionOffset))
 possibleResponses.show()
 
 // Align training data (X) and expected responses (Y) ready for passing to model
 // Because of offsets for past and future data, they cover different time-periods: find the intersection
 val allData = joinedData.join(possibleResponses, List("index", "ticker"), "inner").drop("index").drop("ticker")
 allData.cache()
+allData.printSchema()
 
 // Train a Deep Learning model
 import org.apache.spark.SparkFiles
@@ -95,6 +97,7 @@ import water.support.SparkContextSupport.addFiles
 
 import org.apache.spark.h2o._
 val h2oContext = H2OContext.getOrCreate(sc)
+
 import h2oContext._
 import h2oContext.implicits._
 
@@ -105,7 +108,7 @@ import _root_.hex.deeplearning.DeepLearningModel.DeepLearningParameters
 val dlParams = new DeepLearningParameters()
 dlParams._epochs = 100
 dlParams._train = trainFrame
-dlParams._response_column = 'price_difference
+dlParams._response_column = responseColumnName
 dlParams._variable_importances = true
 // Create a job
 val dl = new DeepLearning(dlParams, Key.make("dlModel.hex"))
@@ -114,7 +117,7 @@ val dlModel = dl.trainModel.get
 val predictionH2OFrame = dlModel.score(allData)('predict)
 val predictionsFromModel = asRDD[DoubleHolder](predictionH2OFrame).collect.map(_.result.getOrElse(Double.NaN))
 
-val expected = allData.select("price_difference")
-val expectedValues = expected.take(20).map(r => r(0))
+val expected = allData.select(responseColumnName)
+val expectedValues = expected.take(20).map(r => r(0).asInstanceOf[Float])
 val comp = (expectedValues zip predictionsFromModel)
 comp.map(i => (i._2-i._1)/i._1).map(math.abs).sum / comp.length
